690d8584f72f59cf68422da444aad117c4198c41
Dave <dave@jut.io>
2015-11-17 15:17:20 -0800

    Make run.sh only run the backends

---
+++ scripts/run-backends.sh
@@ -0,0 +1,2 @@
+./apache-cassandra-2.2.3/bin/cassandra &
+./elasticsearch-2.0.0/bin/elasticsearch &
--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-./apache-cassandra-2.2.3/bin/cassandra & 
-./elasticsearch-2.0.0/bin/elasticsearch &
-node src/orestes.js
