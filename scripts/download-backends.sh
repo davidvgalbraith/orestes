f28468951a90fa29b637f6d45cd7abef62bb4125
Dave <dave@jut.io>
2015-11-17 14:52:11 -0800

    Scripts for downloading and running

---
+++ scripts/download-backends.sh
@@ -0,0 +1,7 @@
+curl -L -O http://apache.mirrorcatalogs.com/cassandra/2.2.3/apache-cassandra-2.2.3-bin.tar.gz
+tar -xf apache-cassandra-2.2.3-bin.tar.gz
+rm apache-cassandra-2.2.3-bin.tar.gz
+
+curl -L -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.0.0/elasticsearch-2.0.0.tar.gz
+tar -xvf elasticsearch-2.0.0.tar.gz
+rm elasticsearch-2.0.0.tar.gz
---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+./apache-cassandra-2.2.3/bin/cassandra & 
+./elasticsearch-2.0.0/bin/elasticsearch &
+node src/orestes.js
